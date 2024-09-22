0eb4effeef6e30654ca5a0fcf31c2c87fc714967
Ishaan Dey <ishaan1013@gmail.com>
2024-09-22 01:19:06 -0400

    glow

--- app/page.tsx
+++ app/page.tsx
@@ -7,39 +7,39 @@ import Footer from "@/components/clubsite/Footer";
 import MousePos from "@/components/clubsite/MousePos";
 
 export default function HomePage() {
-    return (
-        <main className="mx-6 sm:mx-0">
-            <Header defaultPage={0} />
-            <div className="max-w-4xl mx-auto">
-                <div className="h-[calc(100vh-80px)]">
-                    <Landing />
-                </div>
-                <section
-                    id="about"
-                    className="section sm:-mt-16 flex flex-col sm:h-screen"
-                >
-                    <About />
-                </section>
-            </div>
-            <div className="max-w-5xl mx-auto">
-                <section
-                    id="projects"
-                    className="section mt-20 sm:-mt-[150px] flex flex-col sm:h-screen"
-                >
-                    <Projects />
-                </section>
-            </div>
-            <div className="max-w-5xl mx-auto">
-                <section
-                    id="team"
-                    className="section mt-20 sm:-mt-20 sm:scroll-m-[40px]"
-                >
-                    <Team />
-                </section>
-            </div>
-            <MousePos />
+  return (
+    <main className="mx-6 sm:mx-0">
+      <Header defaultPage={0} />
+      <div className="max-w-4xl mx-auto">
+        <div className="h-[calc(100vh-80px)]">
+          <Landing />
+        </div>
+        <section
+          id="about"
+          className="section sm:-mt-16 flex flex-col sm:h-screen"
+        >
+          <About />
+        </section>
+      </div>
+      <div className="max-w-5xl mx-auto">
+        <section
+          id="projects"
+          className="section mt-20 sm:-mt-[150px] flex flex-col sm:h-screen"
+        >
+          <Projects />
+        </section>
+      </div>
+      <div className="max-w-5xl mx-auto">
+        <section
+          id="team"
+          className="section mt-20 sm:-mt-20 sm:scroll-m-[40px]"
+        >
+          <Team />
+        </section>
+      </div>
+      <MousePos />
 
-            <Footer />
-        </main>
-    );
+      <Footer />
+    </main>
+  );
 }
